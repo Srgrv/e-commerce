598a234118301cee78cb3f891bb15728a2caa91d
Sergey Georgiev <sgrgv_art@mail.ru>
2023-06-30 23:32:34 +0300

    повесил метод для добавления в корзину

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+
+//reducers
+import { addToOrder } from "../store/slices/HomeSlice";
 
 const Item = (props) => {
+  const dispatch = useDispatch();
+
   return (
     <div className="item">
       <img src={"./img/" + props.img} alt="" />
       <h2>{props.title}</h2>
       <p>{props.desc}</p>
       <b>{props.price}$</b>
-      <div className="add-to-cart">+</div>
+      <div
+        className="add-to-cart"
+        onClick={() => dispatch(addToOrder({ props }))}
+      >
+        +
+      </div>
     </div>
   );
 };
