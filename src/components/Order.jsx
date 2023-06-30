368b171d60f54f25473a78c930ab34e3d57404a1
Sergey Georgiev <sgrgv_art@mail.ru>
2023-06-30 23:55:00 +0300

    добавление кнопки для удаления

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { FaTrash } from "react-icons/fa";
+
+//reducers
+import { deleteFromOrder } from "../store/slices/HomeSlice";
 
 const Order = (props) => {
+  const dispatch = useDispatch();
   return (
     <div className="item">
       <img src={"./img/" + props.img} alt="" />
       <h2>{props.title}</h2>
       <p>{props.price}$</p>
+      <FaTrash
+        className="delete-icon"
+        onClick={() => dispatch(deleteFromOrder({ props }))}
+      />
     </div>
   );
 };
