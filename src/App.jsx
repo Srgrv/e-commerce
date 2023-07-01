870b95e20ecc952b3b02376f190d7875083b263a
Sergey Georgiev <sgrgv_art@mail.ru>
2023-07-01 16:48:43 +0300

    монтирование компонента

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 
 //components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Items from "./components/Items";
 import Categories from "./components/Categories";
+import ShowFullItem from "./components/ShowFullItem";
 
 function App() {
+  const showFullItem = useSelector((state) => state.home.showFullItem);
+
   return (
     <div className="wrapper">
       <Header />
       <Categories />
       <Items />
+      {showFullItem && <ShowFullItem />}
       <Footer />
     </div>
   );
