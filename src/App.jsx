a27df66e84af19aeffddc265fefedb292d309526
Sergey Georgiev <sgrgv_art@mail.ru>
2023-07-01 15:32:55 +0300

    добавил компоненту в Аpp

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Items from "./components/Items";
+import Categories from "./components/Categories";
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
+      <Categories />
       <Items />
       <Footer />
     </div>
